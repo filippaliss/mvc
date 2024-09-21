27ffc6bd5a82d82810de99d733f16007e969e6bd
Filippa Lissäng Öst <filippaliss@gmail.com>
2024-09-21 15:08:21 +0200

    first commit

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    #[Route("/api/quote", name: "api-qoute")]
+    public function number(): Response
+    {
+        $qoute = [
+        "Im polish, try me russian bitch",
+        "Nomnom betyder stoppa in i munnen",
+        "BSK - Bilar Suger Kuk"
+        ];
+        $randomQoute =$qoute[array_rand($qoute)];
+
+        $response = new JsonResponse(
+            [
+                "quote" => $randomQoute,
+                "date" => date("Y-m-d H-i-s")
+            ]
+        );
+        
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+
+    #[Route("/api", name: "api")]
+    public function api(): Response
+    {
+        return $this->render('api.html.twig');
+    }
+
+}
